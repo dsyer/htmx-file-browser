8ad7dfb292455593322c9bce9a4e97ef22dd5404
Dave Syer <dsyer@vmware.com>
2023-11-24 09:34:47 +0000

    Add folders

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,18 @@
 package com.example.demo;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.jstach.jstache.JStache;
 import io.jstach.jstache.JStachePath;
 import io.jstach.opt.spring.webmvc.JStachioModelView;
-
 
 @SpringBootApplication
 @JStachePath(prefix = "templates/", suffix = ".mustache")
@@ -26,41 +27,83 @@ public class DemoApplication implements WebMvcConfigurer {
 @Controller
 class HomeController {
 	@GetMapping
-	public View home(@ModelAttribute Foo target) {
-		return JStachioModelView.of(new IndexPage(target));
+	public View home() {
+		return JStachioModelView.of(new IndexPage(Folder.cwd()));
 	}
-	
+
 }
 
 @JStache(path = "index")
 class IndexPage {
-	private Foo target;
-	public IndexPage(Foo target) {
-		this.target = target;
+	private Node root;
+
+	public IndexPage(Node root) {
+		this.root = root;
 	}
-	public Foo getTarget() {
-		return target;
+
+	public Node getRoot() {
+		return root;
 	}
-	public void setTarget(Foo target) {
-		this.target = target;
+
+	public void setRoot(Node root) {
+		this.root = root;
 	}
 }
 
-class Foo {
-	private String value;
-	public Foo(String value) {
-		this.value = value;
+class Node implements Comparable<Node> {
+	private String name;
+	private Node parent;
+
+	public Node(String name) {
+		this.name = name;
 	}
-	public String getValue() {
-		return value;
+
+	public String getName() {
+		return name;
 	}
-	public void setValue(String value) {
-		this.value = value;
+
+	public void setName(String name) {
+		this.name = name;
 	}
+
 	@Override
 	public String toString() {
-		return "Foo{" +
-			"value='" + value + "'" +
-			"}";
+		return "Node{" +
+				"name='" + name + "'" +
+				"}";
 	}
+
+	public Node getParent() {
+		return parent;
+	}
+
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		return this.name.compareTo(o.name);
+	}
+}
+
+class Folder extends Node {
+
+	public static Folder ROOT = new Folder("/");
+
+	public static Folder cwd() {
+		return new Folder(".");
+	}
+
+	private SortedSet<Node> children = new TreeSet<>();
+
+	public Folder(String name) {
+		super(name);
+	}
+
+	public void add(Node child) {
+		child.setParent(this);
+		children.add(child);
+	}
+
 }
